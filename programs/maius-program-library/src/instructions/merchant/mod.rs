978d104ee4de430b1673463ae8c16ca441b1e9c3
Thomas Hoang <megatunger@gmail.com>
2022-07-16 21:52:07 +0700

    Update Merchant

--- programs/maius-program-library/src/instructions/merchant/mod.rs
+++ programs/maius-program-library/src/instructions/merchant/mod.rs
@@ -1,3 +1,5 @@
 pub mod initialize_merchant;
+pub mod update_merchant;
 
 pub use initialize_merchant::*;
+pub use update_merchant::*;
--- tests/merchant/index.ts
+++ tests/merchant/index.ts
@@ -1,5 +1,12 @@
-import { airdropAccounts, createNewKeys, initializeMerchant } from "../shared";
+import {
+  airdropAccounts,
+  createNewKeys,
+  initializeMerchant,
+  program,
+} from "../shared";
 import { globalState } from "../maius-program-library";
+import { findCustomerAddress, findMerchantAddress } from "./address";
+import { SystemProgram } from "@solana/web3.js";
 
 export const merchantTests = describe("[Merchant] Test Cases", () => {
   before(async () => {
@@ -9,13 +16,22 @@ export const merchantTests = describe("[Merchant] Test Cases", () => {
   });
   it("[Merchant] Create", async () => {});
 
-  it("[Merchant] Add new customers", async () => {});
+  it("[Merchant] Update", async () => {
+    const merchantWallet = globalState.merchantWallet;
+    const merchantAccount = await findMerchantAddress(merchantWallet);
+    await program.methods
+      .updateMerchant("Maius Pay 1", "Updated", "https://google.com")
+      .accounts({
+        merchantAccount: merchantAccount,
+        merchantAuthority: merchantWallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      })
+      .signers([merchantWallet.payer])
+      .rpc();
 
-  it("[Merchant] Update", async () => {});
-
-  it("[Merchant Customers] Update", async () => {});
-
-  it("[Merchant Customers] Delete", async () => {});
+    const data = await program.account.merchant.fetch(merchantAccount);
+    console.log("After Update: ", data);
+  });
 
   it("[Merchant] Delete", async () => {});
 });
