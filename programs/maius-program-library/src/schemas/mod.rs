239775e6be72959aa1f838d96d864686c8477780
Tuan Pham Minh <tuanpmhd@gmail.com>
2022-07-19 22:07:34 +0700

    resolve conflicts

--- programs/maius-program-library/src/schemas/mod.rs
+++ programs/maius-program-library/src/schemas/mod.rs
@@ -3,10 +3,11 @@ pub mod customer;
 pub mod product;
 pub mod subscription;
 pub mod subscription_item;
+pub mod price;
 
 pub use merchant::*;
 pub use customer::*;
 pub use product::*;
 pub use subscription::*;
 pub use subscription_item::*;
-
+pub use price::*;
